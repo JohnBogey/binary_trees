56f8211441329945bf4e8587ce1d4967a4af4b33
mittsahl <msalem311@aol.com>
2021-03-10 20:05:18 +0000

    Finds if whether a binary tree is balanced

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "binary_trees.h"
+int height(const binary_tree_t *tree);
+
+/**
+* binary_tree_balance - determines if a bianry tree is balanced  
+* @tree: node fo a binary tree 
+* Return: 
+*/
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (height(tree->left) - height(tree->right));
+}
+
+/**
+ * height - finds the height of the binary tree
+ * @tree: root of binary tree
+ * Return: height
+ */
+
+int height(const binary_tree_t *tree)
+{
+	int left, right;
+	
+	if (!tree)
+		return (0);
+	if (!(tree->left) && !(tree->right))
+		return (1);
+	left = height(tree->left);
+	right = height(tree->right);
+	if (left > right)
+		return (1 + left);
+	return (1 + right);
+}
