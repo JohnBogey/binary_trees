38b8f511d671d641e0b37faf827cc60e00bc9dd9
mittsahl <msalem311@aol.com>
2021-03-11 20:18:52 +0000

    Updated height function

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -25,8 +25,11 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_l;
+size_t height_l;
 	size_t height_r;
+
+	if (tree == NULL)
+		return (0);
 
 	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
